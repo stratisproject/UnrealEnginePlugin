14afb810e6bf579a938729c2b9a3120f99130f97
Lazar Prijovic <madrazzl3@gmail.com>
2022-07-27 12:00:38 +0300

    Add Blockchain module entry point

---
+++ Source/Blockchain/Private/BlockchainModule.cpp
@@ -0,0 +1,12 @@
+#include "BlockchainModule.h"
+
+void FBlockchainModule::StartupModule()
+{
+
+}
+
+void FBlockchainModule::ShutdownModule()
+{
+}
+	
+IMPLEMENT_MODULE(FBlockchainModule, Blockchain)
